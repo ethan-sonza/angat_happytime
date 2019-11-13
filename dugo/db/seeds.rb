5818f00570770ffd71787fb96eb4ae63abc747c3
Ethan <ethanjericho@yahoo.com>
2019-11-13 12:41:50 +1000

    more seeds

--- dugo/db/seeds.rb
+++ dugo/db/seeds.rb
@@ -1,27 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-
-User.create({
+user = User.create({
   username: "test",
   password: "test",
   first_name: "Test",
   last_name: "User",
 })
 
-User.first.posts.create({
+user.posts.create({
   longitude: 14.5894,
   latitude: 121.0693547,
   blood_type: "A+",
   blood_amount: 500,
   name: "Test Post",
   age: "35",
-  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ornare erat eu libero fermentum rutrum. 
-    Donec pellentesque nibh id dignissim tristique. Nullam sed blandit nunc, pretium tincidunt erat. Pellentesque consectetur elementum hendrerit. 
-    Integer dapibus, elit suscipit finibus maximus, ligula ligula fringilla dui, quis imperdiet odio arcu ac turpis.",
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ornare erat eu libero fermentum rutrum. Donec pellentesque nibh id dignissim tristique. Nullam sed blandit nunc, pretium tincidunt erat. Pellentesque consectetur elementum hendrerit. Integer dapibus, elit suscipit finibus maximus, ligula ligula fringilla dui, quis imperdiet odio arcu ac turpis.",
+})
+
+user.posts.create({
+  longitude: 12.5894,
+  latitude: 111.06221,
+  blood_type: "O+",
+  blood_amount: 1500,
+  name: "John Jacob",
+  age: "21",
+  description: "Need blood.",
+})
+
+user = User.create({
+  username: "billy",
+  password: "test",
+  first_name: "Billy",
+  last_name: "Bob",
+})
+
+user.posts.create({
+  longitude: 14.5894,
+  latitude: 121.0693547,
+  blood_type: "B-",
+  blood_amount: 2000,
+  name: "Uncle George",
+  age: "58",
+  description: "My uncle lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ornare erat eu libero fermentum rutrum.",
 })
